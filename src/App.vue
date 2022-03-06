b4dc3094c6e12ee1f7f3f8137a81b2dd45d93448
ahmadaldabouqii <aldabouqiahmad@gmail.com>
2022-03-06 14:08:15 +0300

    DC-Hero

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header />
+  <div class="w-full flex">
+    <!-- <DcHeros /> -->
+    <Calendar />
+  </div>
+</template>
+
+<script>
+import Header from './components/AppHeader';
+// import DcHeros from './components/DcHeros';
+import Calendar from './components/Calendar';
+export default {
+  name: 'App',
+  components: { Header, Calendar },
+};
+</script>
